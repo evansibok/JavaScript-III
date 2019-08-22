ec8f334b7d35af1475fc3c16870f1f095f1cad53
evansibok <evansibok@gmail.com>
2019-08-22 03:40:11 +0100

    formatted my this.js file after completion

--- assignments/this.js
+++ assignments/this.js
@@ -1,21 +1,21 @@
 /* The four principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. Global Binding - Whenever "this" is called in a global context it refers to the global window object.
-* 2. New Binding - When "this" is called in a new constructor, "this" refers to the instance of the object called.
-* 3. Implicit Binding - When a function is called by a preceding ".", the object left of the dot gets the "this".
-* 4. Explicit Binding - When "this" keyword is called either using "call", "apply", or "bind" this is explicit binding.
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. Global Binding - Whenever "this" is called in a global context it refers to the global window object.
+ * 2. New Binding - When "this" is called in a new constructor, "this" refers to the instance of the object called.
+ * 3. Implicit Binding - When a function is called by a preceding ".", the object left of the dot gets the "this".
+ * 4. Explicit Binding - When "this" keyword is called either using "call", "apply", or "bind" this is explicit binding.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
 // code example for Window Binding
 
-function sayMyName(name){
-    console.log(this);
-    return this.name;
+function sayMyName(name) {
+  console.log(this);
+  return this.name;
 }
 sayMyName("Evans");
 
@@ -24,53 +24,54 @@ sayMyName("Evans");
 // code example for Implicit Binding
 
 let laugh = {
-    laughing: "I am laughing so hard!",
-    exposeLaugh: function(){
-        console.log(this);
-        return `Hello, I guess ${this.laughing}`;
-    }
-}
+  laughing: "I am laughing so hard!",
+  exposeLaugh: function() {
+    console.log(this);
+    return `Hello, I guess ${this.laughing}`;
+  }
+};
 laugh.exposeLaugh();
 
 // Principle 3
 
 // code example for New Binding
 
-function Teacher(name, course){
-    this.name = name;
-    this.course = course;
+function Teacher(name, course) {
+  this.name = name;
+  this.course = course;
 }
 
-Teacher.prototype.introduction = function(){
-    console.log(`implicit binding  `, this)
-    console.log(`My name is ${this.name} and I teach ${this.course}`)
-}
+Teacher.prototype.introduction = function() {
+  console.log(`implicit binding  `, this);
+  console.log(`My name is ${this.name} and I teach ${this.course}`);
+};
 
-var bimbo = new Teacher('Bimbo', 'Physics');
+var bimbo = new Teacher("Bimbo", "Physics");
 bimbo.introduction();
 
 // Principle 4
 
 // code example for Explicit Binding
 
-function Father(name, age){
-    this.name = name;
-    this.age = age;
-    console.log(this);
-    this.speak = () => {
-        console.log(`Hello! My name is ${this.name} and I am ${this.age} years old`);
-    }
+function Father(name, age) {
+  this.name = name;
+  this.age = age;
+  console.log(`explicit father object   `, this); // I'm not sure how this console logs both the Father and Son objects
+  this.speak = () => {
+    console.log(
+      `Hello! My name is ${this.name} and I am ${this.age} years old`
+    );
+  };
 }
 
-const akpan = new Father('Akpan', 60);
-
+const akpan = new Father("Akpan", 60);
 
 function Son(name, age) {
-    Father.call(this, name, age);
-    // console.log(this)
+  Father.call(this, name, age);
+  console.log(`explicit son object  `, this); // This console logs only the son object
 }
 
-const oduma = new Son('Oduma', 14)
+const oduma = new Son("Oduma", 14);
 
 akpan.speak();
 oduma.speak();
